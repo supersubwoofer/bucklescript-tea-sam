00dd16a055f4043abb5cdbf271e4ed20e819b39b
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2017-09-22 00:49:27 -0400

    add ci debug script

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,6 @@
 import node_resolve from 'rollup-plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 
-//debug
-var fs = require('fs');
-fs.realpath('lib/es6/src/', function(err, path) {
-    if (err) {
-        console.log(err);
-     return;
-    }
-    console.log('Path is : ' + path);
-});
-fs.readdir('lib/es6/src/', function(err, files) {
-    if (err) return;
-    files.forEach(function(f) {
-        console.log('Files: ' + f);
-    });
-});
-//debug
-
 export default {
     entry: './lib/es6/src/main.js',
     dest: './release/main.js',
