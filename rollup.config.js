4b7bbd6b662bb2d6835c3306e2ecb9d5accadb8d
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2017-09-22 17:17:36 -0400

    remove watch on release

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import node_resolve from 'rollup-plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
 
 export default {
     entry: './lib/es6/src/main.js',
@@ -7,12 +6,9 @@ export default {
     format: 'iife',
     plugins: [
         node_resolve({
-            jsnext: true,
-            main: true,
             module: true,
             browser: true,
-        }),
-        livereload('release')
+        })
     ],
     name: 'starter'
 }
