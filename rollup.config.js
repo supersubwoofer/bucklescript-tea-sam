a6606fad57da2eac18f1ef2553f3c16d532cb3e2
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2017-09-21 22:30:05 -0400

    add Rollup local-resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import node_resolve from 'rollup-plugin-node-resolve';
+import localResolve from 'rollup-plugin-local-resolve';
 import livereload from 'rollup-plugin-livereload';
+
 
 export default {
     input: './lib/es6/src/main.js',
@@ -9,6 +11,7 @@ export default {
     },
     plugins: [
         node_resolve({module: true, browser: true}),
+        localResolve(),
         livereload('release')
     ],
     name: 'starter'
