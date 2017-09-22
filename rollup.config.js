d8b01ad1b1c4c6e551bd11cce30333135dcde6ed
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2017-09-21 23:37:44 -0400

    mod rollup-plugin-node-resolve setting

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,13 @@
 import node_resolve from 'rollup-plugin-node-resolve';
-import multiEntry from 'rollup-plugin-multi-entry';
 import livereload from 'rollup-plugin-livereload';
 
 
 export default {
     entry: './lib/es6/src/main.js',
-    input: './lib/es6/src/main.js',
-    output: {
-        file: './release/main.js',
-        format: 'iife'
-    },
+    dest: './release/main.js',
+    format: 'iife',
     plugins: [
         node_resolve({module: true, browser: true}),
-        multiEntry(),
         livereload('release')
     ],
     name: 'starter'
