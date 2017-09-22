6b4a8acc323b4a0ca869e01ff9d329fcc1f5a490
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2017-09-22 01:20:48 -0400

    mod ci debug script

--- ci.debug.js
+++ ci.debug.js
@@ -1,21 +1,15 @@
 var fs = require('fs');
-fs.realpath(__dirname, function(err, path) {
-    if (err) {
-        console.log(err);
-     return;
-    }
-    console.log('Path is : ' + path);
-});
-fs.readdir(__dirname, function(err, files) {
-    if (err) return;
-    files.forEach(function(f) {
-        console.log('Files: ' + f);
-    });
-});
+
 fs.realpath('./lib/es6/src', function(err, path) {
     if (err) {
         console.log(err);
      return;
     }
     console.log('Path is : ' + path);
+});
+fs.readdir('./lib/es6/src', function(err, files) {
+    if (err) return;
+    files.forEach(function(f) {
+        console.log('Files: ' + f);
+    });
 });
