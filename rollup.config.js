874cad1f207d0f451f8a90d9a7eb42740e1f2789
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2017-09-22 14:47:25 -0400

    test to add commonjs rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import node_resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 
 export default {
@@ -6,7 +7,12 @@ export default {
     dest: './release/main.js',
     format: 'iife',
     plugins: [
-        node_resolve({ jsnext: true, main: true }),
+        node_resolve({
+        jsnext: true,
+        main: true,
+        browser: true,
+        }),
+        commonjs(),
         livereload('release')
     ],
     name: 'starter'
